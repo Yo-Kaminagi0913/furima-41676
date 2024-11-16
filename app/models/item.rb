c046d27f463b739de1337bcbfc0cfd11d3c77c34
Yo-Kaminagi0913 <yamazaki1029@yahoo.co.jp>
2024-11-16 23:31:57 +0900

    validationの設定（一部）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,14 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :sales_status
   belongs_to :shipping_fee_status
   belongs_to :prefecture
   belongs_to :scheduled_delivery
   belongs_to :user
-  has_one_attached :image
 
-  validates :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id,
-            :prefecture_id, :scheduled_delivery_id, :price, presence: true
+  has_one_attached :image
+  validates :image, :name, :info, :price, presence: true
+  validates :category_id, :sales_status_id, :shipping_fee_status_id,
+            :prefecture_id, :scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank" }
 end
