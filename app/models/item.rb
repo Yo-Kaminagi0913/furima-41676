3ff84dbe3a9095c4a257e56d3c74832cde469557
Yo-Kaminagi0913 <yamazaki1029@yahoo.co.jp>
2024-11-16 18:27:30 +0900

    ActiveStorageの導入とitemモデルへのvalidationの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,8 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :scheduled_delivery
   belongs_to :user
+  has_one_attached :image
+
+  validates :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id,
+            :prefecture_id, :scheduled_delivery_id, :price, presence: true
 end
