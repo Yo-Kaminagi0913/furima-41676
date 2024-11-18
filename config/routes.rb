77d1556cd8dcc0d4edf70d195eeb6aeb2e485072
Yo-Kaminagi0913 <yamazaki1029@yahoo.co.jp>
2024-11-18 15:13:28 +0900

    微調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:'items#index'
-
   resources :items, only: [:new, :create, :index, :show, :edit, :update]
   # Defines the root path route ("/")
   # root "articles#index"
