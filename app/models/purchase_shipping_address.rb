dd8246a34aab5c25e535902375ee987d5dfcaa5f
Yo-Kaminagi0913 <yamazaki1029@yahoo.co.jp>
2024-11-19 21:17:43 +0900

    PurchaseShippingAddressの修正

--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -1,13 +1,13 @@
 class PurchaseShippingAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :purchase_id, :postal_code, :prefecture_id, :city, :address, :phone_number, :building_name
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :phone_number, :building_name
 
   with_options presence: true do
-    validates :user_id, :item_id, :purchase_id, :city, :address
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
-    validates :phone_number, format: { only_integer: true, with: /\A\d{10,11}\z/, message: 'is invalid. Input only number' }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :user_id, :item_id, :city, :address
+    validates :phone_number, length: { minimum: 10, maximum: 11, message: 'is too short' }, format: { only_integer: true, with: /\A\d{10,11}\z/, message: 'is invalid. Input only number' }
   end
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
