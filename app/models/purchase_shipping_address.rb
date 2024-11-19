7be1e69cfcaaee252a7546cc0ae0b3c8efc2bd13
Yo-Kaminagi0913 <yamazaki1029@yahoo.co.jp>
2024-11-19 21:23:52 +0900

    validationの微調整

--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -6,7 +6,7 @@ class PurchaseShippingAddress
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :user_id, :item_id, :city, :address
-    validates :phone_number, length: { minimum: 10, maximum: 11, message: 'is too short' }, format: { only_integer: true, with: /\A\d{10,11}\z/, message: 'is invalid. Input only number' }
+    validates :phone_number, format: { only_integer: true, with: /\A\d{10,11}\z/, message: 'is invalid. Input only number' }
   end
 
   def save
