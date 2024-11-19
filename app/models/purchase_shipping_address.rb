aae5067ed2041a17a2b45b992e7d8a745e45c8ec
Yo-Kaminagi0913 <yamazaki1029@yahoo.co.jp>
2024-11-19 19:57:00 +0900

    Formオブジェクトpurchase_shipping_addressの作成とバリデーション・データ保存処理の記述

---
+++ app/models/purchase_shipping_address.rb
@@ -0,0 +1,17 @@
+class PurchaseShippingAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :purchase_id, :postal_code, :prefecture_id, :city, :address, :phone_number, :building_name
+
+  with_options presence: true do
+    validates :user_id, :item_id, :purchase_id, :city, :address
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
+    validates :phone_number, format: { only_integer: true, with: /\A\d{10,11}\z/, message: 'is invalid. Input only number' }
+  end
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    ShippingAddress.create(purchase_id: purchase.id, postal_code: postal_code, prefecture_id: prefecture_id, city: city,
+                           address: address, phone_number: phone_number, building_name: building_name)
+  end
+end
