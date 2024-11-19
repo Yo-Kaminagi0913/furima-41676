2817578c91fa4ad666787e1f34d1353128154312
Yo-Kaminagi0913 <yamazaki1029@yahoo.co.jp>
2024-11-20 01:16:09 +0900

    クレジットカードでの決済機能を実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,16 +1,20 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!, only: :index
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @purchase_shipping_address = PurchaseShippingAddress.new
     @item = Item.find(params[:item_id])
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @purchase_shipping_address = PurchaseShippingAddress.new(purchase_params)
     if @purchase_shipping_address.valid?
+      pay_item
       @purchase_shipping_address.save
       redirect_to root_path
     else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render :index, status: :unprocessable_entity
     end
   end
@@ -19,6 +23,16 @@ class PurchasesController < ApplicationController
 
   def purchase_params
     params.require(:purchase_shipping_address).permit(:postal_code, :prefecture_id, :city, :address, :phone_number,
-                                                      :building_name).merge(user_id: current_user.id, item_id: params[:item_id])
+                                                      :building_name).merge(user_id: current_user.id,
+                                                                            item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -1,8 +1,9 @@
 class PurchaseShippingAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :phone_number, :building_name
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :phone_number, :building_name, :token
 
   with_options presence: true do
+    validates :token
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :user_id, :item_id, :city, :address
